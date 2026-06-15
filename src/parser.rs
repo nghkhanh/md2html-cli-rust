7e33a731dd23c52f610876dd599a6ea877e5a443
khanhnh <khanhsave001@gmail.com>
2026-06-15 13:06:32 +0700

    feat: implement parse_inline_elements to process content for h1, h2, and paragraph tags

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,7 @@
+fn parse_inline_elements(line: &str) -> String {
+    line.to_string()
+}
+
 pub fn parse(markdown_input: &str) -> String {
 
     let mut html_output = String::new();
@@ -6,19 +10,22 @@ pub fn parse(markdown_input: &str) -> String {
 
     for line in lines {
         if line.starts_with("## "){
-            let content = line.strip_prefix("## ").unwrap();
+            let raw_content = line.strip_prefix("## ").unwrap();
+            let processed_content = parse_inline_elements(raw_content);
             let h2_tag = format!("<h2>{}</h2>\n", content);
 
             html_output.push_str(&h2_tag);
         }
         else if line.starts_with("# "){
-            let content = line.strip_prefix("# ").unwrap();
+            let raw_content = line.strip_prefix("# ").unwrap();
+            let processed_content = parse_inline_elements(raw_content);
             let h1_tag = format!("<h1>{}</h1>\n", content);
 
             html_output.push_str(&h1_tag);
         }
         else {
             if !line.is_empty() {
+                let processed_content = parse_inline_elements(line);
                 let p_tag = format!("<p>{}</p>\n", line);
                 html_output.push_str(&p_tag);
             }
